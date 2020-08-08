df1b260e44fb73db4b8218e529e7e291199fe4d7
doan-nguyenduc <67990216+doan-nguyenduc@users.noreply.github.com>
2020-08-08 12:35:27 +0700

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,5 +3,6 @@
 int main()
 {
   printf("Hello Jenkins This messsage printed from C program.  Polling build trigger what the food!!!  \n");
+  printf("Hello Jenkins This messsage printed from C program.  Polling build trigger what the food!!!  \n");
   return 0;
 }
