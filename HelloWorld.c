333f51c9de9feae00abc31f0f0c62880d95aa0ed
doan-nguyenduc <67990216+doan-nguyenduc@users.noreply.github.com>
2020-08-08 12:22:30 +0700

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-  printf("Hello Jenkins This messsage printed from C program. This SCO Polling build trigger \n");
+  printf("Hello Jenkins This messsage printed from C program. This SCO Polling build trigger  \n");
   return 0;
 }
