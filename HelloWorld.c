6f2e540d58569161f97591ab19dcadf8060f457f
LAPTOP-CKRTJ121\doan <doan.uit@gmail.com>
2020-08-08 21:33:15 +0700

    this update from master

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,5 +4,6 @@ int main()
 {
   printf("Hello Jenkins This messsage printed from C program.  Polling build trigger what the food!!!  \n");  
   printf("This testing for branch git");
+  //Update from master
   return 0;
 }
